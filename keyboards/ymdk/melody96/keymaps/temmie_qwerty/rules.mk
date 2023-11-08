ce3780a9bc26f5c12d6c9d43cd4d3891dfaf744a
Ouidkling - Domicile <matteo.nadler.campou@gmail.com>
2023-11-08 23:21:30 +0100

    new rules + new layout syntax

--- keyboards/ymdk/melody96/keymaps/temmie_qwerty/rules.mk
+++ keyboards/ymdk/melody96/keymaps/temmie_qwerty/rules.mk
@@ -1,18 +1,2 @@
-# Generic features
-BOOTMAGIC_ENABLE   = full
-COMMAND_ENABLE     = yes
-CONSOLE_ENABLE     = yes
-EXTRAKEY_ENABLE    = yes
-MOUSEKEY_ENABLE    = no
-NKRO_ENABLE        = yes
-TAP_DANCE_ENABLE   = no
-UNICODEMAP_ENABLE  = no
-
-# Keyboard-specific features
-BACKLIGHT_ENABLE   = no
-RGBLIGHT_ENABLE    = yes
-
-# Firmware size reduction
-GRAVE_ESC_ENABLE   = no
-MAGIC_ENABLE       = no
-SPACE_CADET_ENABLE = no
+VIA_ENABLE = yes
+LTO_ENABLE = yes
